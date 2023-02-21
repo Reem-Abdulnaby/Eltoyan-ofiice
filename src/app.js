2076fc754448728e3038482881c88948b25e9d43
Reem-Abdulnaby <reemabdulnaby00@gmail.com>
2023-02-21 23:43:34 +0200

    start

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+const path=require('path')
+const PORT=process.PORT || 3000
+require('./connection/db')
+app.use(express.json())
+
+app.use('/images',express.static(path.join(__dirname, "./src/uploads")))
+const serviceRouter=require('./router/servoce-router')
+const clientRouter=require('./router/client-router')
+const requestRouter=require('./router/request-router')
+app.use(serviceRouter)
+app.use(clientRouter)
+app.use(requestRouter)
+app.listen(PORT,()=>{console.log('server is run on port 3000')})
---
+++ src/model/services.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose')
+const serviceSchema=mongoose.Schema({
+
+    title:{
+        type:String,
+        trim:true,
+        required:true,
+    },
+    description:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    image:{
+        type:String,
+        
+    }
+     
+})
+serviceSchema.methods.toJSON=function(){
+    const service=this
+    const seviceObj=service.toObject()
+    return seviceObj
+}
+ const Service=mongoose.model('services',serviceSchema)
+module.exports=Service
